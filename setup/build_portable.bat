7755cfff8b40ac14e2f9a170a69ebc4f7a630e03
strayge <strayge@gmail.com>
2019-12-01 00:29:08 +0300

    added script for archive portables

---
+++ setup/build_portable.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\7-Zip\7z.exe" a -mx9 ../bin/Release/tray_monitor_binaries.zip ../bin/Release/CpuIcon.exe ../bin/Release/DiskIcon.exe ../bin/Release/NetIcon.exe ../bin/Release/RamIcon.exe ../bin/Release/BatteryIcon.exe
