243778b71b4304df51938d0c8bf9f18c97be5e20
strayge <strayge@gmail.com>
2019-11-30 23:59:55 +0300

    removed unused usages

--- src/RamIcon/CustomTrayIcon.cs
+++ src/RamIcon/CustomTrayIcon.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using System.Diagnostics;
 using IconLibrary;
 using static TrayIconLibrary.WinApi;
